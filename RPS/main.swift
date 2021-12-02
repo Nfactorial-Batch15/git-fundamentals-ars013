773a47351e8962782720e569ce5c6e39598b1a01
Arslan Assankulov <arslan.assankulov@gmail.com>
2021-12-02 18:30:25 +0600

    Initial Commit

--- RPS/main.swift
+++ RPS/main.swift
@@ -7,5 +7,40 @@
 
 import Foundation
 
-print("Hello, World!")
+print("Please make your choice!")
 
+enum Choises: Int {
+    case rock = 1
+    case paper = 2
+    case scissors = 3
+}
+
+let name = readLine()!
+
+func sw(x: String) -> Int{
+    if x == "rock" {
+        return 1
+    }
+    else if x == "paper" {
+        return 2
+    }
+    else {
+        return 3
+    }
+}
+
+var n = sw(x: name)
+
+var randomInt = Int.random(in: 1...3)
+
+func emp(x: Int, y: Int) -> String{
+    if x == 1 && y == 3 || x == 2 && y == 1 || x == 3 && y == 2{
+        return "You won!"
+    }
+    
+    else {
+        return "Computer won!"
+    }
+}
+
+print(emp(x: n, y: randomInt))
